02d7210e13e6919977f7b088d6217739929737a8
Jerry Hogan <jerry@enyata.com>
2021-10-23 21:46:53 +0100

    Main content in progress

--- src/components/PhotoOfTheDay/ContentsPage.jsx
+++ src/components/PhotoOfTheDay/ContentsPage.jsx
@@ -4,18 +4,18 @@ const Contentspage = ({ photoOfTheDay, peopleAlso }) => {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 lg:gap-24 my-4">
             <div className="col-span-12 lg:col-span-7">
-                <header className="py-3 text-xl font-bold text-black">
+                <header className="py-3 text-2xl lg:text-xl font-bold text-black">
                     About the {photoOfTheDay.name}
                 </header>
-                <h3 className="text-text font-bold text-lg">
+                <h3 className="text-text font-bold text-xl lg:text-lg">
                     Pets
                 </h3>
 
-                <p className="text-base text-text leading-6 my-2 pr-14">
+                <p className="text-base text-text leading-6 my-2 pr-4 lg:pr-14">
                     {photoOfTheDay.details.description}
                 </p>
 
-                <p className="text-base text-text leading-6 mt-6 pr-14">
+                <p className="text-base text-text leading-6 mt-6 pr-4 lg:pr-14">
                     text to mockup various fonts for a type specimen book.So how did the classical Latin become so incoherent? According to McClintock.
                 </p>
 
